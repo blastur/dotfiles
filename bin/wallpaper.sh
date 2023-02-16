1a1aacfa60c160a525268feaeff5dc03b1bc9be7
Magnus Olsson <magnus@minimum.se>
2023-02-16 21:36:48 +0100

    wallpaper.sh: Add "fixed" mode
    Adds wallpaper mode to always use ~/.config/bg.jpg.

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -9,24 +9,47 @@ WALLPAPER_MODE=${WALLPAPER_MODE:-#101010}
 WALLPAPER_DIR=${WALLPAPER_DIR:-$HOME/.config/wallpapers}
 
 random_wallpaper() {
-    dir=$*
+    local dir=$*
 
     if [ ! -d "${dir}" ]; then
         echo "error: dir '${dir}' doesn't exist, cannot select wallpaper" >&2
-        exit 1
+        solid_color
     fi
 
-    find "${dir}" |sort -R | tail -n1 | while read -r file; do
+    find "${dir}" | sort -R | tail -n1 | while read -r file; do
         echo "Setting random wallpaper '${file}'"
         feh --no-fehbg --bg-fill "${file}"
     done
 }
 
+solid_color() {
+    if ! xsetroot -solid "${WALLPAPER_MODE}"; then
+        echo "error: failed to set solid color ${WALLPAPER_MODE}" >&2
+        exit 1
+    fi
+}
+
+fixed_wallpaper() {
+    local bg="${HOME}/.config/bg.jpg"
+    if [ -e "${bg}" ]; then
+        feh --no-fehbg --bg-fill "${bg}"
+    else
+        solid_color
+    fi
+}
+
 case "${WALLPAPER_MODE}" in
-    \#*)
-        xsetroot -solid "${WALLPAPER_MODE}"
+    fixed)
+        fixed_wallpaper
         ;;
     random)
         random_wallpaper "${WALLPAPER_DIR}"
+        ;;
+    \#*)
+        solid_color
+        ;;
+    *)
+        echo "error: bad wallpaper mode ${WALLPAPER_MODE}" >&2
+        exit 1
         ;;
 esac
